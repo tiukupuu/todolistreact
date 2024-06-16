c4bdf6c350d8fda5b2954e7404ed39896bd80425
Tiia Puumalainen <tiia.puumalainen@edu.karelia.fi>
2024-06-16 18:31:56 +0300

    Additions

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+
 import App from './App';
+
 
 test('renders App component', () => {
   render(<App />);
