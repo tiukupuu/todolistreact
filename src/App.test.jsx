40271b76d738486365193602a94ce6b4c2aa9f85
Tiia Puumalainen <tiia.puumalainen@edu.karelia.fi>
2024-06-16 17:51:42 +0300

    Code modified

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,8 +8,9 @@ test('renders App component', () => {
   expect(header).toBeInTheDocument();
 });
 
-test('add todo', () => {
+test('add and clear todos', () => {
   render(<App />);
+
 
   const desc = screen.getByPlaceholderText('Description');
   fireEvent.change(desc, { target: { value: 'Go to coffee' } });
@@ -17,9 +18,18 @@ test('add todo', () => {
   fireEvent.change(date, { target: { value: '29.12.2023' } });
   const status = screen.getByPlaceholderText('Status');
   fireEvent.change(status, { target: { value: 'Open' } });
-  const button = screen.getByText('Add');
-  fireEvent.click(button);
+  const addButton = screen.getByText('Add');
+  fireEvent.click(addButton);
+
 
   const table = screen.getByRole('table');
   expect(table).toHaveTextContent('Go to coffee');
+
+
+  const clearButton = screen.getByText('Clear');
+  fireEvent.click(clearButton);
+
+
+  expect(table).not.toHaveTextContent('Go to coffee');
 });
+
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/https://github.com/tiukupuu/todolistreact.git/',
   plugins: [react()],
   test: {
     globals: true,
